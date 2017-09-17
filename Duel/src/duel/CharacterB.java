8af1621de68d9917dda8a89c4ceae20c89a79db6
Siddeeq <Siddeeq@DESKTOP-RIF12TV.home>
2017-09-17 15:13:08 -0400

    implemtented necessary methods

--- Duel/src/duel/CharacterB.java
+++ Duel/src/duel/CharacterB.java
@@ -10,12 +10,33 @@ public class CharacterB implements Dueler{
 		return "Sid";
 	}
 	public void taunt() {
-		System.out.println("You're a mistake.");
+		System.out.println("You're a mistake");
 	}
 	public void setStartingHP(int hp) {
 		this.hp = hp;
 	}
 	public int getHP() {
 		return hp;
+	}
+	public boolean determineIfOpponentIsFair(Dueler d, int hp) {
+		if (d.getHP()==hp)
+		return true;
+		else
+		return false;
+		
+	}
+	public int getAction(Object caller) {
+		if (caller != this)
+			return 0;
+		else
+			// to be edited later
+			return 0;
+	}
+
+	public void hit(Object caller) {
+		if (caller instanceof Duel) {
+			this.hp = this.hp-10;
+		}
+		
 	}
 }
